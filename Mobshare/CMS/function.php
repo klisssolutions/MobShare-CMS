aa312d06c1ed2d0c53d4cd4d19b3f59cafba5dc6
Time Kliss Solutions <48053167+klisssolutions@users.noreply.github.com>
2019-04-05 07:55:10 -0300

    pasta upload
    enviando a função de cadastro de imagens e a pasta de uploads

---
+++ Mobshare/CMS/function.php
@@ -0,0 +1,38 @@
+<?php
+    
+    //PUXANDO A FOTO
+
+    function imagens($item){
+        $foto = $item['name'];
+        $tamanho_foto = $item['size'];
+        $tamanho_foto = round($tamanho_foto/1024);
+        $ext_foto = strrchr($foto, ".");
+        $nome_foto = pathinfo($foto, PATHINFO_FILENAME);
+        $nome_foto = md5(uniqid(time()).$nome_foto);
+        $diretorio = "upload/";
+        $extensao = array(".jpg",".png",".jpeg");
+
+        if(in_array($ext_foto, $extensao)){
+            if($tamanho_foto<=2000){
+                $foto_tmp = $item['tmp_name'];
+                $arquivo = $diretorio.$nome_foto.$ext_foto;
+                
+                if(move_uploaded_file($foto_tmp, $arquivo)){
+                    return $arquivo;
+                    
+                    
+                }else{
+                    $arquivo = null;
+                    return $arquivo;
+                    
+                }
+                return $arquivo;
+            }
+        }
+
+        return $arquivo;
+    }
+        
+        
+
+?>
