f90102141210fc61eb579a781b64683a27d79391
Time Kliss Solutions <48053167+klisssolutions@users.noreply.github.com>
2019-04-29 14:11:17 -0300

    api de anuncios

---
+++ Mobshare/controller/controllerAnuncios.php
@@ -0,0 +1,44 @@
+<?php
+/*
+    Projeto: MobShare
+    Autor: Emanuelly
+    Data Criação: 29/04/2019
+    Data Modificação:
+    Conteudo Modificação: 
+    Autor da Modificação: 
+    Objetivo da classe: Controller da API para Anuncios
+*/
+
+@session_start();
+require_once($_SESSION["importInclude"]); 
+
+class controllerTermos{
+
+    public function __construct(){
+        //Import da classe funcionario
+        require_once(IMPORT_ANUNCIOS);
+
+        //Import da classe funcionarioDAO, para inserir no BD
+        require_once(IMPORT_ANUNCIOS_DAO);
+    }
+
+
+    public function listarAnuncio(){
+        //Instancia do DAO criado para ser usado em todos os outros métodos
+        $anuncios = new anunciosDAO();
+        return($anunciosDAO->selectAll());
+    }
+
+
+
+    public function buscarAnuncio(){
+        $id = $_GET["id"];
+
+        $anunciosDAO = new anunciosDAO();
+
+        return $anunciosDAO->selectById($id);
+
+    
+    }
+}
+?>
