fccad30ad05264cd4bbc8b6ae58b6dea3ab7bede
Time Kliss Solutions <48053167+klisssolutions@users.noreply.github.com>
2019-04-29 21:41:35 -0300

    arrumar o como funciona do site

--- Mobshare/SITE/view/comofunciona/comofunciona.php
+++ Mobshare/SITE/view/comofunciona/comofunciona.php
@@ -1,8 +1,18 @@
-<?php
-@session_start();
-@$_SESSION["importInclude"] = $_SERVER["DOCUMENT_ROOT"] . "/Mobshare/include.php";
-require_once($_SESSION["importInclude"]);
+<?php 
+
+    @session_start();
+    @$_SESSION["importInclude"] = $_SERVER["DOCUMENT_ROOT"] . "/Mobshare/CMS/include.php";
+    require_once($_SESSION["importInclude"]); 
+    require_once(IMPORT_FUNCIONAMENTO);
+    require_once(IMPORT_FUNCIONAMENTO_CONTROLLER);
+
+    $controllerFuncionamento = new controllerFuncionamento();
+
+    $funcionamentos[] = new Funcionamento();
+    $funcionamentos = $controllerFuncionamento->listarFuncionamento();
+
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,64 +26,50 @@ require_once($_SESSION["importInclude"]);
     <?php require_once(HEADER);?>
     <!-- CAIXA QUE SEGURA O CONTEÚDO EMBAIXO DO MENU -->
     <section class="content">
-        <div class="box-como-funciona-primeiro">
-            <div class="texto-como-funciona-pag">
-                <p>Para usar o site, é só fazer o login e se não tiver cadastro faça ele que é simples.
-                    Depois disso veja o carro disponivel mais perto de você e converse diretamento com quem está
-                    alugando o carro. Não esqueça que você também pode colocar um carro pra alugar!
-                </p>
-                <h1>Venha usar nosso site!</h1>
-            </div>
-            <div class="image-como-funciona-pag">
-                    <img src="../images/carrodinheiro.png" width="700" height="450"  alt="App da MobShare">
-            </div>
-        </div>
+        <?php
+            $cont = 0;
+                foreach($funcionamentos as $funcionamento){
+                    if($funcionamento->getAtivo()){
+                        if($cont%2){
+        ?>
+        
         <div class="box-como-funciona-segundo">
             <div class="image-como-funciona2">
-                    <img src="../images/carrogente.png" width="700" height="400"  alt="App da MobShare">
+                    <img src="<?php echo("/Mobshare/arquivos/".$funcionamento->getFoto());?>" width="700" height="400"  alt="App da MobShare">
             </div>
             <div class="texto-como-funciona2">
-                    <h1>Venha usar nosso site!</h1>
-                    <p>Para usar o site, é só fazer o login e se não tiver cadastro faça ele que é simples.
-                        Depois disso veja o carro disponivel mais perto de você e converse diretamento com quem está
-                        alugando o carro. Não esqueça que você também pode colocar um carro pra alugar!
-                    </p>
+                    <h1><?php echo($funcionamento->getTitulo());?></h1>
+                    <p><?php echo($funcionamento->getDescricao());?></p>
                    
             </div>
         </div>
-        
+                        <?php $cont++; }else{?>
         <div class="box-como-funciona-quarto">
             
             <div class="texto-como-funciona4">
-                    <h1>Venha usar nosso site!</h1>
-                    <p>Para usar o site, é só fazer o login e se não tiver cadastro faça ele que é simples.
-                            Depois disso veja o carro disponivel mais perto de você e converse diretamento com quem está
-                            alugando o carro. Não esqueça que você também pode colocar um carro pra alugar!
-                    </p>
+                    <h1><?php echo($funcionamento->getTitulo());?></h1>
+                    <p><?php echo($funcionamento->getDescricao());?></p>
                 
                     
             </div>
             <div class="image-como-funciona4">
-                <img src="../images/carrochat.png" width="700" height="400"  alt="App da MobShare">
+                <img src="<?php echo("/Mobshare/arquivos/".$funcionamento->getFoto());?>" width="700" height="400"  alt="App da MobShare">
 
         </div>
+</div>
+                    <?php 
+                $cont++;    
+                }
+                    }
+                }?>
         
-        </div>
-        <div class="box-como-funciona-terceiro">
-            <div class="avaliacoes">
-                    <h1>★★★★☆</h1>
-                    <p>Para usar o site, é só fazer o login e se não tiver cadastro faça ele que é simples.
-                            Depois disso veja o carro disponivel mais perto de você e converse diretamento com quem está
-                            alugando o carro. Não esqueça que você também pode colocar um carro pra alugar!
-                </p>
-                <h2>- Alex Gonçalvez</h2>
-            </div>
-        </div>
-
+        
+        
+        
+        
     </section>
     <!-- RODAPÉ-->
     <?php require_once(FOOTER);?>
-    
     </body>
     
     </html>
