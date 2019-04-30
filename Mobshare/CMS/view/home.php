69fb343e4d909aa6dcfb2a0531788396c7e8c773
Time Kliss Solutions <48053167+klisssolutions@users.noreply.github.com>
2019-04-29 21:46:33 -0300

    Ajustar palavras para bater com as wireframes-KAIO

--- Mobshare/CMS/view/home.php
+++ Mobshare/CMS/view/home.php
@@ -1,21 +1,6 @@
 <?php
-    @session_start();
-    require_once($_SESSION["importInclude"]);
-    require_once(IMPORT_FUNCIONARIO);
-    require_once(IMPORT_NIVEL);
-    require_once(IMPORT_FUNCIONARIO_CONTROLLER);
-    require_once(IMPORT_NIVEL_CONTROLLER);
+   
 
-    $funcionario = new Funcionario();
-    $nivel = new Nivel();
-    $funcionarioController = new controllerFuncionario();
-    $nivelController = new controllerNivel();
-
-    $_GET["id"] = $_SESSION['idFuncionario'];
-    $funcionario = $funcionarioController->buscarFuncionario();
-    $_GET["id"] = $funcionario->getIdNivel();
-    $nivel = $nivelController->buscarNivel();
-    $permissoes = $nivel->getPermissoes();
 ?>
 <html>
 
@@ -35,7 +20,7 @@
         <div class="cabecalho">
 
             <div class="logo">
-                <a href="#" onclick="inicio()">
+                <a href="#">
                     <img src="view/imagens/logo.png" class="logo" heigth="150" width="200">
                 </a>
             </div>
@@ -43,70 +28,60 @@
             <div class="administrador">
             <div class="icone"><img src="view/imagens/settings.png" heigth="32" width="32"></div>
                 <div class="texto">
-                    <h1><?php echo($funcionario->getNome());?></h1>
-                    <p><?php echo($nivel->getNome());?></p>
+                    <h1>Leonardo Cavalcante</h1>
+                    <p>Administrador</p>
                 </div>
                 
             </div>
         </div>
         <div class="conteudo">
             <nav>
-                <?php if(acessoModulo($permissoes, MODULO_FUNCIONARIO)){?>
-                    <a href="#" onclick="gerenciarFuncionario();">
-                        <div class="link">
-                            Gerenciamento de Funcionarios
-                        </div>
-                    </a>
-                <?php }?>
-                <?php if(acessoModulo($permissoes, MODULO_MARKETING)){?>
-                    <a href="#" onclick="emailMarketing();">
-                        <div class="link" >
-                            Gerenciamento de E-mail Marketing
-                        </div>
-                    </a>
-                <?php }?>
-                <?php if(acessoModulo($permissoes, MODULO_LOCACAO)){?>
-                    <a href="#" onclick="historicoLocacao();">
-                        <div class="link">
-                            Gerenciamento Histórico de Locação
-                        </div>
-                    </a>
-                <?php }?>
-                <?php if(acessoModulo($permissoes, MODULO_PAGINA)){?>
-                    <a href="#" onclick="admPaginas();">
-                        <div class="link" >
-                        Administração das Paginas do Site
-                        </div>
-                    </a>
-                <?php }?>
-                <?php if(acessoModulo($permissoes, MODULO_APROVACAO)){?>
-                    <a href="#" onclick="aprovacao();">
-                        <div class="link">
-                            Aprovação de Cadastro
-                        </div>
-                    </a>
-                <?php }?>
-                <?php if(acessoModulo($permissoes, MODULO_CANCELAMENTO)){?>
-                    <a href="#" onclick="aprovacaoCancelamento();">
-                        <div class="link">
-                            Aprovação de Cancelamento
-                        </div>
-                    </a>
-                <?php }?>
-                <?php if(acessoModulo($permissoes, MODULO_MODELO)){?>
-                    <a href="#" onclick="marcaModelo();">
-                        <div class="link">
-                            Gerenciamento de Marcas e Modelos
-                        </div>
-                    </a>
-                <?php }?>                   
+                <a href="#" onclick="gerenciarFuncionario();">
+                    <div class="link">
+                        Gerenciamento de Funcionarios
+                    </div>
+                </a>
+                <a href="#" onclick="emailMarketing();">
+                    <div class="link" >
+                        Gerenciamento de E-mail Marketing
+                    </div>
+                </a>
+                <a href="#" onclick="historicoLocacao();">
+                    <div class="link">
+                        Gerenciamento Histórico de Locação
+                    </div>
+                </a>
+                <a href="#" onclick="faleConosco();">
+                    <div class="link">
+                        Gerenciamento de Marcas e Modelos
+                    </div>
+                </a>
+                <a href="#" onclick="admPaginas();">
+                    <div class="link" >
+                    Administração das Paginas do Site
+                    </div>
+                </a>
+                <a href="#" onclick="aprovacao();">
+                    <div class="link">
+                        Aprovação de Cadastro
+                    </div>
+                </a>
+                <a href="#" onclick="relatorio();">
+                    <div class="link">
+                        Relatório
+                    </div>
+                </a>
                 <div class="botao">
                     <a href="index.php?destroy"><input class="btn-sair" type="button" value="Logout" id="logout"></a>
                 </div>
             </nav>
             <div class="menu" id="menu">
                 <div class="informacao" id="informacao">
-                <script>inicio();</script>
+                    <div class="texto-bem-vindo">
+                        <img src="view/imagens/settings.png" heigth="150" width="150">
+                        <h1>Administração de Conteúdo</h1>
+                        <p>Selecione as opções no menu ao lado!</p>
+                    </div>
                 </div>
             </div>
         </div>
