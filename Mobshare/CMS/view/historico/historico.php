1d0bb3316527e276c7e05945b6e0406bd51b55d1
Time Kliss Solutions <48053167+klisssolutions@users.noreply.github.com>
2019-05-02 19:47:31 -0300

    Arrumando Design KAIO

--- Mobshare/CMS/view/historico/historico.php
+++ Mobshare/CMS/view/historico/historico.php
@@ -1,69 +1,82 @@
-<div class="titulo">GERENCIAMENTO DO HISTORICO DE LOCAÇÃO</div>
+<?php 
 
-<table>
+  @session_start();
+  require_once($_SESSION["importInclude"]); 
+
+  require_once(IMPORT_V_DETALHES_LOCACAO);
+  require_once(IMPORT_V_DETALHES_LOCACAO_CONTROLLER);
+
+  $controllerV_detalhes_locacao = new controllerV_detalhes_locacao();
+  $locacoes[] = new V_detalhes_locacao();
+  $locacoes = $controllerV_detalhes_locacao->listarLocacoes();
+?>
+
+<div class="titulo-func-lista">GERENCIAMENTO DO HISTORICO DE LOCAÇÃO</div>
+
+<table class="func-cad">
         <tr>
-            <td class="label2">
+            <td class="titulo-func-cad">
                 Locador:
             </td>
-            <td class="input2">
+            <td class="titulo-func-cad">
               pessoa1
             </td>
         </tr>
         <tr>
-            <td class="label2">
+            <td class="titulo-func-cad">
                 Locatario:
             </td>
-            <td class="input2">
+            <td class="titulo-func-cad">
               pessoa2
             </td>
         </tr>
         <tr>
-            <td class="label2">
+            <td class="titulo-func-cad">
                 Data:
             </td>
-            <td class="input2">
+            <td class="titulo-func-cad">
               15/03/2019
             </td>
         </tr>
         <tr>
-            <td class="label2">
+            <td class="titulo-func-cad">
                 Inicio:
             </td>
-            <td class="input2">
+            <td class="titulo-func-cad">
               13:30
             </td>
         </tr>
         <tr>
-            <td class="label2">
+            <td class="titulo-func-cad">
                 Fim:
             </td>
-            <td class="input2">
+            <td class="titulo-func-cad">
               17:30
             </td>
         </tr>
         <tr>
-            <td class="label2">
+            <td class="titulo-func-cad">
                 Veiculo:
             </td>
-            <td class="input2">
+            <td class="titulo-func-cad">
               Audi R8
             </td>
         </tr>
 
         <tr>
-            <td class="label2">
+            <td class="titulo-func-cad">
                 Valor:
             </td>
-            <td class="input2">
+            <td class="titulo-func-cad">
               R$50,50
             </td>
         </tr>
         <tr>
-            <td class="label2">
-               <input type="button" value="Voltar" class="botao3" onclick="historicoLocacao();">
+            <td class="titulo-func-cad">
+               <input type="button" value="Voltar" class="btn-fun" onclick="historicoLocacao();">
             </td>
-            <td class="input2">
-              <input type="submit" value="Salvar" class="botao3">
+            <td class="titulo-func-cad">
+              
             </td>
         </tr>
     </table>
