d91912e4a9a1bd983933cf7c5d6eff59c0f4ead4
Time Kliss Solutions <48053167+klisssolutions@users.noreply.github.com>
2019-05-07 23:59:48 -0300

    adicionando controller KAIO

---
+++ Mobshare/controller/controllerEndereco.php
@@ -0,0 +1,98 @@
+<?php
+/*
+    Projeto: MobShare
+    Autor: Kaio
+    Data Criação: 07/05/2019
+    Data Modificação:
+    Conteudo Modificação:
+    Autor da Modificação:
+    Objetivo da classe: Controller de endereços
+*/
+
+(@session_start());
+require_once($_SESSION["importInclude"]);
+
+class controllerEndereco{
+
+    public function __construct(){
+        //Import da classe nivel
+        require_once(IMPORT_ENDERECO);
+
+        //Import da classe nivelDAO, para inserir no BD
+        require_once(IMPORT_ENDERECO_DAO);
+    }
+
+    public function inserirCategorias(){
+        //Instancia do DAO criado para ser usado em todos os outros métodos
+        $modeloDAO = new modeloDAO();
+        
+        //Verifica qual metodo esta sendo requisitado do formulario(POST ou GET)
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $nomeModelo = $_POST["txtModelo"];
+            $marca = $_POST["cbbMarca"];
+
+            //Instancia da classe
+            $modelo = new Modelo();
+
+            //Guardando os dados do post no objeto da classe
+            $modelo->setNomeModelo($nomeModelo);
+            $modelo->setIdMarca($marca);
+
+            /* Chamada para o metodo de inserir no BD, passando como parâmetro o objeto
+            marcaClass que tem todos os dados que serão inseridos no banco de dados */
+            return $modeloDAO->insert($modelo);
+        }
+    }
+
+    public function excluirCategorias(){
+        //Instancia do DAO
+        $modeloDAO = new modeloDAO();
+        //Esse id foi enviado pela view no href, o arquivo de rota é quem chamou este método
+        $id = $_GET["id"];
+
+        //Chamada para o método de excluir um nivel
+        return $modeloDAO->delete($id);
+    }
+
+
+    public function atualizarCategorias(){
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+            $id = $_GET["id"];
+            $nomeModelo = $_POST["txtModelo"];
+            $idMarca = $_POST["cbbMarca"];
+
+            //Instancia da classe
+            $modelo = new Modelo();
+            $modeloDAO = new modeloDAO();
+
+            //Guardando os dados do post no objeto da classe
+            $modelo->setIdModelo($id);
+            $modelo->setNomeModelo($nomeModelo);
+            $modelo->setIdMarca($idMarca);
+            
+            
+            /* Chamada para o metodo de inserir no BD, passando como parâmetro o objeto
+            contatoClass que tem todos os dados que serão inseridos no banco de dados */
+            return $modeloDAO->update($modelo);
+        }
+    }
+
+    public function buscarCategorias(){
+        //ARRUMAR A BUSCA para a pagina inicial
+        $id = 1;
+
+        $modeloDAO = new modeloDAO();
+        
+        return $modeloDAO->selectById($id);
+    }
+
+      
+
+    public function listarEnderecos(){
+        //Instancia do DAO
+        $enderecoDAO = new enderecoDAO();
+        return $enderecoDAO->selectAll();
+    }
+}
+?>
