c6576c10f705a378bcba8339689feaaa11c63280
Time Kliss Solutions <48053167+klisssolutions@users.noreply.github.com>
2019-05-08 00:19:30 -0300

    arquivo js com ajax KAIO

--- Mobshare/SITE/view/js/link.js
+++ Mobshare/SITE/view/js/link.js
@@ -9,3 +9,28 @@ function cadastrarAluguel() {
 function veiculo() {
     location.href = "usuarioVeiculo.php";
 }
+
+function router(controller, modo, id){
+
+    let form = document.getElementById("form");
+    let checkForm = form.reportValidity();
+    let data = new FormData(form);
+    if(checkForm){
+        
+        $('#form').submit(function(event) {
+            event.preventDefault();
+            $.ajax({
+                type: "POST",
+                processData: false,
+                contentType: false,
+                cache: false,
+                url: `router.php?controller=${controller}&modo=${modo}&id=${id}`,
+                data: data,
+                success: function (data) {                    
+                    
+                    $("#informacao").html(data);
+                }
+            });
+        });
+    }
+}
