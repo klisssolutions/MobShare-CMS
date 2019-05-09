0c013659c60cb12e5b0e77160abccfbdfc56abdf
unknown <leonardp_cavalcante.07@hotmail.com>
2019-05-09 06:59:52 -0300

    leo

---
+++ Mobshare/model/DAO/acessorioDAO.php
@@ -0,0 +1,92 @@
+<?php
+/*
+    Projeto: MobShare
+    Autor: Emanuelly
+    Data Criação: 29/04/2019
+    Data Modificação:
+    Conteudo Modificação:
+    Autor da Modificação:
+    Objetivo da classe: DAO para a API
+*/
+//Import do arquivo de constantes
+@session_start();
+require_once($_SESSION["importInclude"]); 
+ 
+
+class acessorioDAO{
+    private $conex;
+
+    public function __construct(){
+        //Import da classe do banco para todos os métodos
+        require_once(IMPORT_CONEXAO);
+
+        //Instancia da classe conexão
+        $this->conex = new conexaoMySQL();
+    }
+
+
+
+    public function selectByVeiculo($idVeiculo
+    ){
+        $sql = "select a.idAcessorio, a.idTipoVeiculo, a.nomeAcessorio from Acessorio as a join Acessorio_Veiculo as av 
+        on av.idAcessorio = a.idAcessorio join Veiculo as v on 
+        av.idVeiculo = v.idVeiculo where v.idVeiculo = ".$idVeiculo;
+
+        //Abrindo conexão com o BD
+        $PDO_conex = $this->conex->connectDataBase();
+
+        //executa o script de select no bd
+        $select = $PDO_conex->query($sql);
+        $cont = 0;
+        
+        /* $select->fetch no formado pdo retorna os dados do BD
+        também retorna com característica do PDO como o fetch
+        é necessário especificar o modelo de conversão.
+        EX: PDO::FETCH_ASSOC, PDO::FETCH_ARRAY etc. */
+        $listAcessorios[] = new Acessorio();
+        $listAcessorios = null;
+        while($rsAcessorio=$select->fetch(PDO::FETCH_ASSOC)){
+            $acessorio = new Acessorio();
+            $acessorio->setIdAcessorio($rsAcessorio["idAcessorio"]);
+            $acessorio->setIdTipoVeiculo($rsAcessorio["idTipoVeiculo"]);
+            $acessorio->setNomeAcessorio($rsAcessorio["nomeAcessorio"]);
+            
+
+            $listAcessorios[$cont] = $acessorio;
+            $cont++;
+        }
+
+        $this->conex->closeDataBase();
+
+        return($listAcessorios);
+
+    }
+
+       //Seleciona um registro pelo ID.
+       public function selectById($id){
+        $sql = SELECT . VIEW_ANUNCIOS . " WHERE idVeiculo=".$id;
+
+        //Abrindo conexão com o BD
+        $PDO_conex = $this->conex->connectDataBase();
+
+        //executa o script de select no bd
+        $select = $PDO_conex->query($sql);
+
+        /* $select->fetch no formado pdo retorna os dados do BD
+        também retorna com característica do PDO como o fetch
+        é necessário especificar o modelo de conversão.
+        EX: PDO::FETCH_ASSOC, PDO::FETCH_ARRAY etc. */
+        if($rsAnuncios=$select->fetch(PDO::FETCH_ASSOC)){
+            $anuncios = new anuncios();
+            $anuncios->setIdVeiculo($rsAnuncios["idVeiculo"]);
+            $anuncios->setNomeModelo($rsAnuncios["nomeModelo"]);
+            $anuncios->setNomeMarca($rsAnuncios["nomeMarca"]);
+            $anuncios->setFotoVeiculo($rsAnuncios["fotoVeiculo"]);
+        }
+
+        $this->conex->closeDataBase();
+
+        return($anuncios);
+    }
+}
+?>
---
+++ Mobshare/model/acessorioClass.php
@@ -0,0 +1,80 @@
+<?php
+/*
+    Projeto: MobShare
+    Autor: Leonardo
+    Data Criação: 08/05/2019
+    Data Modificação:
+    Conteudo Modificação:
+    Autor da Modificação:
+    Objetivo da classe: Modelar acessório
+*/
+class Acessorio{
+    private $idAcessorio;
+    private $idTipoVeiculo;
+    private $nomeAcessorio;
+    
+
+
+
+    /**
+     * Get the value of idVeiculo
+     */ 
+    public function getIdAcessorio()
+    {
+        return $this->idAcessorio;
+    }
+
+    /**
+     * Set the value of idVeiculo
+     *
+     * @return  self
+     */ 
+    public function setIdAcessorio($idAcessorio)
+    {
+        $this->idAcessorio = $idAcessorio;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idVeiculo
+     */ 
+    public function getIdTipoVeiculo()
+    {
+        return $this->idTipoVeiculo;
+    }
+
+    /**
+     * Set the value of idVeiculo
+     *
+     * @return  self
+     */ 
+    public function setIdTipoVeiculo($idTipoVeiculo)
+    {
+        $this->idTipoVeiculo = $idTipoVeiculo;
+
+        return $this;
+    }    
+
+    /**
+     * Get the value of idVeiculo
+     */ 
+    public function getNomeAcessorio()
+    {
+        return $this->nomeAcessorio;
+    }
+
+    /**
+     * Set the value of idVeiculo
+     *
+     * @return  self
+     */ 
+    public function setNomeAcessorio($nomeAcessorio)
+    {
+        $this->nomeAcessorio = $nomeAcessorio;
+
+        return $this;
+    }        
+    
+}
+?>
