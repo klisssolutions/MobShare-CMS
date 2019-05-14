ab5eb0b6f428c2b4ae3031e4de2834006b906a95
Time Kliss Solutions <48053167+klisssolutions@users.noreply.github.com>
2019-05-14 20:03:33 -0300

    atualizando foto veiculo KAIO

--- Mobshare/SITE/view/js/link.js
+++ Mobshare/SITE/view/js/link.js
@@ -10,6 +10,37 @@ function veiculo() {
     location.href = "usuarioVeiculo.php";
 }
 
+function cadastrarImagemVeiculo(categoria, idVeiculo) {
+    $.ajax({
+        type: "POST",
+        url: `cadastroImagem.php?id=${categoria}&idVeiculo=${idVeiculo}`,
+        success: function (dados) {
+            $("#informacao").html(dados);
+        }
+    });
+}
+
+function acessorio() {
+    $.ajax({
+        type: "POST",
+        url: `cadastroAcessorio.php`,
+        success: function (dados) {
+            $("#informacao").html(dados);
+        }
+    });
+}
+
+function paginaInicial(){
+    $.ajax({
+        type: "POST",
+        url: `usuario-inicio/inicioUsuario.php`,
+        success: function (dados) {
+            $("#informacao").html(dados);
+        }
+    });
+}
+
+
 function router(controller, modo, id){
 
     let form = document.getElementById("form");
@@ -24,7 +55,7 @@ function router(controller, modo, id){
                 processData: false,
                 contentType: false,
                 cache: false,
-                url: `router.php?controller=${controller}&modo=${modo}&id=${id}`,
+                url: `../../router.php?controller=${controller}&modo=${modo}&id=${id}`,
                 data: data,
                 success: function (data) {                    
                     
