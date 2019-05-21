68b18b4ebc3f4ada4571914025d1ae64d4e91f3b
Time Kliss Solutions <48053167+klisssolutions@users.noreply.github.com>
2019-05-20 23:46:12 -0300

    Add files via upload

--- Mobshare/SITE/view/js/link.js
+++ Mobshare/SITE/view/js/link.js
@@ -10,6 +10,10 @@ function veiculo() {
     location.href = "usuarioVeiculo.php";
 }
 
+function endereco(){
+    location.href = "cadastroEndereco.php";
+}
+
 function cadastrarImagemVeiculo(categoria, idVeiculo) {
     $.ajax({
         type: "POST",
@@ -40,6 +44,40 @@ function paginaInicial(){
     });
 }
 
+function cadastrarEndereco(){
+    $.ajax({
+        type: "POST",
+        url: `enderecoUsuario.php`,
+        success: function (dados) {
+            $(".content").html(dados);
+        }
+    });
+}
+
+function rota(controller, modo, id){
+
+    let form = document.getElementById("form");
+    let checkForm = form.reportValidity();
+    let data = new FormData(form);
+    if(checkForm){
+        
+        $('#form').submit(function(event) {
+            event.preventDefault();
+            $.ajax({
+                type: "POST",
+                processData: false,
+                contentType: false,
+                cache: false,
+                url: `../../router.php?controller=${controller}&modo=${modo}&id=${id}`,
+                data: data,
+                success: function (data) {                    
+                    
+                    $(".content").html(data);
+                }
+            });
+        });
+    }
+}
 
 function router(controller, modo, id){
 
@@ -63,5 +101,33 @@ function router(controller, modo, id){
                 }
             });
         });
+    }
+}
+
+function selectRouter(controller, modo, id){
+    
+    $.ajax({
+        type: "POST",
+        url: `../../router.php?controller=${controller}&modo=${modo}&id=${id}`,
+        success: function (dados) {
+            $("#informacao").html(dados);
+        }
+    });
+}
+
+function preview(imagem) {
+    console.log(imagem.value)
+    if(imagem.value){
+        let reader = new FileReader();
+
+        reader.onload = function (e) {
+            // get loaded data and render thumbnail.
+            document.getElementById("prev").src = e.target.result;
+        };
+
+        // read the image file as a data URL.
+        reader.readAsDataURL(imagem.files[0]);
+    }else{
+        document.getElementById("prev").src = "";
     }
 }
